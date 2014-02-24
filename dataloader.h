ee9ee52c28ccbe2d70121cba6fb3f469b7ad970e
Alexander Bean Apmann <caboose200@gmail.com>
2014-02-24 16:31:46 -0500

    fix semi colon

--- dataloader.h
+++ dataloader.h
@@ -1,12 +1,14 @@
 #ifndef __DATA_LOADER__
 #define __DATA_LOADER__
 
+#define MAX_INSTANCES 8124
+
 #include <fstream>
 #include <string>
 #include <mushroom.h>
 
-data_Mushroom * loadData(std::string filename, data_Mushroom *arr, numInstances = 8124);
+data_Mushroom * loadData(std::string filename, data_Mushroom *arr, numInstances = MAX_INSTANCES);
 
-data_Mushroom loadInput(std::string filename)
+data_Mushroom loadInput(std::string filename);
 
 #endif

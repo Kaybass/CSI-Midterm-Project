0c7fd0fb5b4a85710291ea8d0e0a01163be42402
Alexander Bean Apmann <caboose200@gmail.com>
2014-02-24 20:17:04 -0500

    Finished mushroom.h, still needs proper weights for mushroom characteristics.

--- mushroom.h
+++ mushroom.h
@@ -1,3 +1,11 @@
+/*
+ *
+ *
+ *
+ *
+*/
+
+
 #ifndef __MUSHROOM__
 #define __MUSHROOM__
 
@@ -21,82 +29,198 @@ enum _cap_surface{
 
 enum _cap_color{
 
+	BROWN = 0,
+	BUFF,
+	CINNAMON,
+	GRAY,
+	GREEN,
+	PINK,
+	PURPLE,
+	RED,
+	WHITE,
+	YELLOW
 };
 
 enum _bruises{
 
+	BRUISED = 0,
+	NOT_BRUISED
 };
 
 enum _odor{
 
+	ALOMND = 0,
+	ANISE,
+	CRESOTE,
+	FISHY,
+	FOUL,
+	MUSTY,
+	NONE,
+	PUGENT,
+	SPICY
 };
 
 enum _gill_attachment{
 
+	ATTACHED,
+	DESCENDING,
+	FREE,
+	NOTCHED
 };
 
 enum _gill_spacing{
 
+	CLOSE,
+	CROWDED,
+	DISTANT
 };
 
 enum _gill_size{
 
+	BROAD,
+	NARROW
 };
 
 enum _gill_color{
 
+	BLACK,
+	BROWN,
+	BUFF,
+	CHOCOLATE,
+	GRAY,
+	GREEN,
+	ORANGE,
+	PINK,
+	PURPLE,
+	RED,
+	WHITE,
+	YELLOW
 };
 
 enum _stalk_shape{
 
+	ENLARGING,
+	TAPERING
 };
 
 enum _stalk_root{
 
+	BULBOUS,
+	CLUB,
+	CUP,
+	EQUAL,
+	RHIZOMORPHS,
+	ROOTED,
+	MISSING
 };
 
 enum _stalk_surface_above_ring{
 
+	FIBROUS,
+	SCALY,
+	SILKY,
+	SMOOTH
 };
 
 enum _stalk_surface_below_ring{
 
+	FIBROUS,
+	SCALY,
+	SILKY,
+	SMOOTH
 };
 
 enum _stalk_color_above_ring{
 
+	BROWN,
+	BUFF,
+	CINNAMON,
+	GRAY,
+	ORANGE,
+	PINK,
+	RED,
+	WHITE,
+	YELLOW
 };
 
 enum _stalk_color_below_ring{
 
+	BROWN,
+	BUFF,
+	CINNAMON,
+	GRAY,
+	ORANGE,
+	PINK,
+	RED,
+	WHITE,
+	YELLOW
 };
 
 enum _veil_type{
 
+	PARTIAL,
+	UNIVERSAL
 };
 
 enum _veil_color{
 
+	BROWN,
+	ORANGE,
+	WHITE,
+	YELLOW
 };
 
 enum _ring_number{
 
+	NONE,
+	ONE,
+	TWO
 };
 
 enum _ring_type{
 
+	COBWEBBY,
+	EVANESCENT,
+	FLARING,
+	LARGE,
+	NONE,
+	PENDANT,
+	SHEATHING,
+	ZONE
 };
 
 enum _spore_print_color{
 
+	BLACK,
+	BROWN,
+	BUFF,
+	CHOCOLATE,
+	GREEN,
+	ORANGE,
+	PURPLE,
+	WHITE,
+	YELLOW
 };
 
 enum _population{
 
+	ABUNDANT,
+	CLUSTERED,
+	NUMEROUS,
+	SCATTERED,
+	SEVERAL,
+	SOLITARY
 };
 
 enum _habitat{
 
+	GRASSES,
+	LEAVES,
+	MEADOWS,
+	PATHS,
+	URBAN,
+	WASTE,
+	WOODS
 };
 
 typedef struct{
